4605c74d47c719cbdb97ec514cf557e20d856bea
chinmaik95 <ckulkarni@albany.edu>
2019-12-06 21:24:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.navigationdrawer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            dl = findViewById(R.id.activity_main);
+            t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+            dl.addDrawerListener(t);
+            t.syncState();
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       // nv = findViewById(R.id.nv);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+}
